26e788c0d981c9eeb762bb4178e6f3505bcaa393
hamzi76 <hamziqandeel@gmail.com>
2025-12-29 17:51:10 +0500

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ function getConfig() {
     return fs.readJsonSync(configPath);
   } catch {
     return {
-      BOTNAME: 'SARDAR RDX',
+      BOTNAME: 'ZAIBI PAPPU',
       PREFIX: '.',
-      ADMINBOT: ['100009012838085'],
+      ADMINBOT: ['61578560588461'],
       TIMEZONE: 'Asia/Karachi',
       PREFIX_ENABLED: true,
       REACT_DELETE_EMOJI: '😡',
